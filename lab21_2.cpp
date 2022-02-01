cff6cc33a568daeb726ca0ea5473a1c73066d8f0
BestChennn <bestchenbestchen@gmail.com>
2022-02-01 14:08:00 +0700

    Compiete

--- lab21_2.cpp
+++ lab21_2.cpp
@@ -5,8 +5,17 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *a,Rect *b){
+	double L1,L2,sum;
 
+	L1 = min((a->x)+(a->w),(b->x)+(b->w)) - max(a->x,b->x);
+	L2 = min((a->y),(b->y))  - max((a->y)-(a->h),(b->y)-(b->h));
+	sum = L1*L2;
+	if(sum<0){
+		return 0;
+	}else{
+		return sum;
+	}
 
 
 }
